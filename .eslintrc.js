8a72c1af47f41b13d2e7c68498db29f33b5089ce
Leandro Machado <lefmachado@gmail.com>
2023-12-09 21:15:50 -0300

    Segundo commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  plugins: ['react', 'prettier'],
+
+  rules: {
+    'react-in-jsx-scope': 'off',
+  },
+};
